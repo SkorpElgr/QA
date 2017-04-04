c6e5202edf3b7b89672b173be4d423df10a91725
paul.skorodzievskiy <paul.skorodzievskiy@gmail.com>
2017-04-04 20:21:31 +0100

    update waiter

--- src/test/java/com/searchtest/webdriver/InitTest.java
+++ src/test/java/com/searchtest/webdriver/InitTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 
@@ -21,6 +23,7 @@ public class InitTest {
     public void start() {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver2_28.exe");
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 20);
     }
 
