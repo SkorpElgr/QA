356c39fcb4fff363a3bf2fc1c1d69f48bb2dff82
paul.skorodzievskiy <paul.skorodzievskiy@gmail.com>
2017-04-10 22:05:01 +0300

    update waiter

--- src/test/java/com/searchtest/webdriver/InitTest.java
+++ src/test/java/com/searchtest/webdriver/InitTest.java
@@ -9,14 +9,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Severity;
+import ru.yandex.qatools.allure.annotations.Title;
+import ru.yandex.qatools.allure.model.SeverityLevel;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-
+@Title("Init Allure test suite class")
 public class InitTest extends TestBaseMain {
-
+    @Severity(SeverityLevel.BLOCKER)
+    @Title("Init Allure test")
+    @Description("Just to make sure allure works)")
     @Test
     public void myFirstTest() {
         driver.navigate().to("https://www.google.com.ua");
