28bfe3954c7fb0ba3ac9d3e86bfc1a7b07a74cbe
pskorodzievskiy <paul.skorodzievskiy@gmail.com>
2017-04-17 00:56:07 +0300

    update

--- src/test/java/com/searchtest/webdriver/TestBaseMain.java
+++ src/test/java/com/searchtest/webdriver/TestBaseMain.java
@@ -3,12 +3,17 @@ package com.searchtest.webdriver;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
@@ -24,6 +29,7 @@ public class TestBaseMain {
     @Before
     public void start() {
         browserName = "Chrome";
+//      browserName = "FirefoxOld";
         if (tlDriver.get() != null) {
             driver = tlDriver.get();
             wait = new WebDriverWait(driver, 10);
@@ -38,18 +44,28 @@ public class TestBaseMain {
                 tlDriver.set(driver);
                 wait = new WebDriverWait(driver, 10);
                 break;
-//            case "Firefox":
-//                caps.setCapability(FirefoxDriver.MARIONETTE, false)
-//                driver = new FirefoxDriver(caps);
-//                break;
-//            case "IE":
-//                System.setProperty("webdriver.ie.driver", "drivers/IEDriverServer.exe");
-//                caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-//                caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
-//                driver = new InternetExplorerDriver(caps);
-//                break;
+            case "FirefoxOld":
+                caps.setCapability(FirefoxDriver.MARIONETTE, false);
+                driver = new FirefoxDriver(caps);
+                tlDriver.set(driver);
+                break;
+            case "FirefoxNightly":
+                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+               caps.setCapability(FirefoxDriver.MARIONETTE, true);
+                driver = new FirefoxDriver(new FirefoxBinary(
+                        new File("C:\\Program Files\\Nightly\\firefox.exe")),
+                        new FirefoxProfile(),caps);
+                tlDriver.set(driver);
+                break;
+            case "IE":
+                System.setProperty("webdriver.ie.driver", "drivers/IEDriverServer.exe");
+                caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+                caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+                driver = new InternetExplorerDriver(caps);
+                tlDriver.set(driver);
+                break;
         }
-        driver.manage().deleteAllCookies();
+//        driver.manage().deleteAllCookies();
         driver.manage().window().setSize(new Dimension(1280,1024));
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 20);
