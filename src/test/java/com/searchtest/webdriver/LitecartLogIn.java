400160dac2f54b65596786e96f6f50496b10f410
pskorodzievskiy <paul.skorodzievskiy@gmail.com>
2017-04-16 23:56:51 +0300

    update

--- src/test/java/com/searchtest/webdriver/LitecartLogIn.java
+++ src/test/java/com/searchtest/webdriver/LitecartLogIn.java
@@ -1,16 +1,13 @@
 package com.searchtest.webdriver;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
-
 public class LitecartLogIn extends TestBaseMain {
-    //http://localhost/litecart/admin/
+
     @Test
     public void LitecardLogIn() {
-        try{
+        try {
             driver.navigate().to("http://localhost/litecart/admin/");
 //            driver.findElement(By.xpath("//form[@name='login_form']//input[@type='password']"))
             driver.findElement(By.xpath("//input[@name='username']")).clear();
@@ -19,8 +16,7 @@ public class LitecartLogIn extends TestBaseMain {
             driver.findElement(By.xpath("//input[@type='password']")).sendKeys("admin");
             driver.findElement(By.xpath("//input[@name='remember_me']")).click();
             driver.findElement(By.xpath("//button[@name='login']")).click();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
